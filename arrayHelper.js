06c4576e2b98f3d04c493bd8d27bc73fa0507b4c
Muscipular <admin@muscipular.net>
2013-08-04 13:45:19 +0800

    add Array find and Date format

--- arrayHelper.js
+++ arrayHelper.js
@@ -27,6 +27,52 @@ var args2Array = function (args, start, end) {
     return Array.prototype.slice.call(args, start, end);
 };
 
+var findItem = function (array, field, expression, binaryFind, desc) {
+    var canBinaryFind = false;
+    if (!array || array.length == 0) {
+        return null;
+    }
+
+    if (typeof expression !== 'function') {
+        expression = (function (value) {
+            return function (obj) {
+                return obj && obj[field] == value;
+            }
+        })(expression);
+    }
+
+    if (binaryFind) {
+        canBinaryFind = expression(array[0], array[0][field]);
+        canBinaryFind = typeof canBinaryFind === 'number' || canBinaryFind instanceof Number;
+    }
+
+    var _binaryFind = function (start, end) {
+        if (start > end) {
+            return null;
+        }
+        var m = (start + end) >> 1;
+        var r = expression(array[m], array[m][field]);
+        if (r < 0) {
+            return desc ? _binaryFind(start, m - 1) : _binaryFind(m + 1, end);
+        } else if (r > 0) {
+            return desc ? _binaryFind(m + 1, end) : _binaryFind(start, m - 1);
+        }
+        return array[m];
+    };
+
+    if (binaryFind && canBinaryFind) {
+        return _binaryFind(0, array.length - 1);
+    }
+
+    for (var i = array.length - 1, item; i >= 0; i--) {
+        if ((item = array[i]) && expression(item, item[field])) {
+            return item;
+        }
+    }
+    return null;
+};
+
 exports.forEach = forEach;
 exports.joinEx = joinEx;
 exports.args2Array = args2Array;
+exports.findItem = findItem;
--- common.js
+++ common.js
@@ -13,3 +13,70 @@ exports.parseFloat = function (value, defaultValue, radix) {
     }
     return value;
 };
+
+exports.dateFormat = function (date, mask) {
+    var fillZero = function (value, length) {
+        if (!length) length = 2;
+        value = String(value);
+        for (var i = 0, zeros = ''; i < (length - value.length); i++) {
+            zeros += '0';
+        }
+        return zeros + value;
+    };
+    return mask.replace(/"[^"]*"|'[^']*'|\b(?:d{1,4}|m{1,4}|yy(?:yy)?|([hHMstT])\1?|[lLZ])\b/g, function ($0) {
+        switch ($0) {
+            case 'd':
+                return date.getDate();
+            case 'dd':
+                return fillZero(date.getDate());
+            case 'ddd':
+                return ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'][date.getDay()];
+            case 'dddd':
+                return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];
+            case 'M':
+                return date.getMonth() + 1;
+            case 'MM':
+                return fillZero(date.getMonth() + 1);
+            case 'MMM':
+                return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()];
+            case 'MMMM':
+                return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][date.getMonth()];
+            case 'yy':
+                return String(date.getFullYear()).substr(2);
+            case 'yyyy':
+                return date.getFullYear();
+            case 'h':
+                return date.getHours() % 12 || 12;
+            case 'hh':
+                return fillZero(date.getHours() % 12 || 12);
+            case 'H':
+                return date.getHours();
+            case 'HH':
+                return fillZero(date.getHours());
+            case 'm':
+                return date.getMinutes();
+            case 'mm':
+                return fillZero(date.getMinutes());
+            case 's':
+                return date.getSeconds();
+            case 'ss':
+                return fillZero(date.getSeconds());
+            case 'l':
+                return fillZero(date.getMilliseconds(), 3);
+            case 'L':
+                var m = date.getMilliseconds();
+                if (m > 99) m = Math.round(m / 10);
+                else m = 0;
+                return fillZero(m);
+            case 'tt':
+                return date.getHours() < 12 ? 'am' : 'pm';
+            case 'TT':
+                return date.getHours() < 12 ? 'AM' : 'PM';
+            case 'Z':
+                return date.toUTCString().match(/[A-Z]+$/);
+            // Return quoted strings with the surrounding quotes removed
+            default:
+                return $0.substr(1, $0.length - 2);
+        }
+    });
+};
